dd8014c6fb580eff178bb2f18200f2ee375ff114
Supun Sulakshana <supunsulakshana2001@gmail.com>
2025-07-01 23:05:56 +0530

    Update TimerInterceptor.java

--- J2EE-Interceptors/src/main/java/lk/jiat/web/interceptor/interceptor/TimerInterceptor.java
+++ J2EE-Interceptors/src/main/java/lk/jiat/web/interceptor/interceptor/TimerInterceptor.java
@@ -7,15 +7,21 @@ import jakarta.interceptor.Interceptor;
 import jakarta.interceptor.InvocationContext;
 import lk.jiat.web.interceptor.anotation.TimeoutLogger;
 
+import java.util.Timer;
+
 @TimeoutLogger
 @Interceptor
 @Priority(1)
 public class TimerInterceptor {
 
     @AroundTimeout
-    public Object aroundTimeout(InvocationContext ic) throws Exception{
+    public Object aroundTimeout(InvocationContext ic) throws Throwable{
         System.out.println("Timer interceptor . aroundTimeout");
-        return ic.proceed();
+
+        Object proceed = ic.proceed();
+
+
+        return proceed;
     }
 
 }
